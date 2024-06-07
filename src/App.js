52da8a157e2187363f89884db2a85da8b2fb07e2
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-06-07 17:53:10 +0300

    files updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
+import PokemonForm from 'components/PokemonForm';
+import PokemonInfo from 'components/PokemonInfo';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default class App extends Component {
+  state = {
+    pokemonName: '',
+  };
+
+  handleFormSubmit = pokemonName => {
+    this.setState({ pokemonName: pokemonName });
+  };
+
   render() {
-    return <div style={{ maxWidth: 1170, margin: '0 auto', pading: 20 }}></div>;
+    return (
+      <div style={{ maxWidth: 1170, margin: '0 auto', pading: 20 }}>
+        <PokemonForm onSubmit={this.handleFormSubmit} />
+        <PokemonInfo pokemonName={this.state.pokemonName} />
+        <ToastContainer autoClose={3000} />
+      </div>
+    );
   }
 }
--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 
 export default class PokemonInfo extends Component {
+  state = {
+    pokemon: null,
+  };
+
+  componentDidUpdate(prevProps, _) {
+    const prevName = prevProps.pokemonName;
+    const nextName = this.props.pokemonName;
+
+    if (prevName !== nextName) {
+      console.log('Name was changed!');
+
+      fetch(`http://pokeapi.co/api/v2/pokemon/${nextName}`)
+        .then(response => response.json())
+        .then(console.log);
+    }
+  }
+
   render() {
-    return <div>Pokemon Info</div>;
+    return (
+      <div>
+        <h1>Pokemon Info</h1>
+        <p>{this.props.pokemonName}</p>
+      </div>
+    );
   }
 }
