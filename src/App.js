096c1e89a9c47cce48aa42c7c4a4329c757c1242
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-06-07 12:28:02 +0300

    files updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-function App() {
-  return <div>App</div>;
-}
+import React, { Component } from 'react';
 
-export default App;
+export default class App extends Component {
+  render() {
+    return <div style={{ maxWidth: 1170, margin: '0 auto', pading: 20 }}></div>;
+  }
+}
