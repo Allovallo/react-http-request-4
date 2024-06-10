95f180f57303e41ee73f9790211c6f9a5a9c5965
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-06-10 12:11:02 +0300

    files updated

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class PokemonInfo extends Component {
   state = {
     pokemon: null,
-    loading: false,
     error: null,
+    status: 'idle',
   };
 
   componentDidUpdate(prevProps, _) {
@@ -12,41 +12,46 @@ export default class PokemonInfo extends Component {
     const nextName = this.props.pokemonName;
 
     if (prevName !== nextName) {
-      this.setState({ loading: true, pokemon: null });
+      this.setState({ status: 'pending' });
 
       fetch(`http://pokeapi.co/api/v2/pokemon/${nextName}`)
         .then(response => {
           if (response.ok) {
             return response.json();
           }
-          return Promise.reject(new Error(`ERROR! No ${nextName} name was found!`));
+          return Promise.reject(new Error(`Errorr in ${nextName} name!!!`));
         })
-        .then(pokemon => this.setState({ pokemon }))
-        .catch(error => this.setState({ error }))
-        .finally(() => this.setState({ loading: false }));
+        .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
+        .catch(error => this.setState({ error, status: 'rejected' }));
     }
   }
 
   render() {
-    const { pokemon, loading, error } = this.state;
-    const { pokemonName } = this.props;
+    if (this.state.status === 'idle') {
+      return <div>Enter pokemon`s name!</div>;
+    }
 
-    return (
-      <div>
-        {error && <h1>{error.message}</h1>}
-        {loading && <div>...Loading...</div>}
-        {!pokemonName && <div>Enter pokemon`s name!</div>}
-        {pokemon && (
+    if (this.state.status === 'pending') {
+      return <div>...Loading...</div>;
+    }
+
+    if (this.state.status === 'rejected') {
+      return <div>{this.state.error.message}</div>;
+    }
+
+    if (this.state.status === 'resolved') {
+      return (
+        <div>
           <div>
-            <p>{pokemon.name}</p>
+            <p>{this.state.pokemon.name}</p>
             <img
-              src={pokemon.sprites.other['official-artwork'].front_default}
+              src={this.state.pokemon.sprites.other['official-artwork'].front_default}
               width="240"
               alt="pokemon-name"
             />
           </div>
-        )}
-      </div>
-    );
+        </div>
+      );
+    }
   }
 }
